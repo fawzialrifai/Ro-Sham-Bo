896dd301f5aad10e1b3ab4613c320ce6b9e9d4dc
Fawzi <fawzialrifai@icloud.com>
2022-04-09 19:03:41 +0300

    Initial Commit

---
+++ Ro-Sham-Bo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ro-Sham-Bo
+//
+//  Created by Fawzi Rifai on 09/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ro-Sham-Bo/Ro_Sham_BoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ro_Sham_BoApp.swift
+//  Ro-Sham-Bo
+//
+//  Created by Fawzi Rifai on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Ro_Sham_BoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
